cd71890ba1f7edecb2942c752aaa4f0b243146c0
z382196829 <1936851315@qq.com>
2021-12-07 01:36:58 +0800

    第十八次提交

---
+++ DigitSum.c
@@ -0,0 +1,24 @@
+﻿#include<stdio.h>
+#include<windows.h>
+
+int DigitSum(int n)
+{
+    int sum = 0;
+    if(n != 0)
+    {
+	sum = n % 10 + DigitSum(n / 10);
+    }
+    return sum;
+}
+
+int main()
+{
+    int i = 0;
+    int ret = 0;
+    printf("请输入：i = ");
+    scanf("%d", &i);
+    ret = DigitSum(i);
+    printf("ret = %d\n", ret);
+    system("pause");
+    return;
+}
